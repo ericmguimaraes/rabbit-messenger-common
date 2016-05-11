b89df6843a94e2d56fbf457bb9d273dbfda78b3e
Saulo Lordão A. Barros <saulo.lordao@gmail.com>
2016-05-11 18:25:28 -0300

    Tela de login.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>RabbitMessenger</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/rabbitmessenger.css">
+  </head>
+  <body class="php">
+    <div class="container">
+      <div class="background"></div>
+      <div class="login-panel">
+        <div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-2">
+          <div class="account-wall text-center">
+            <img class="img-responsive" src="assets/img/logo_login.jpg" alt="">
+            <form class="form-horizontal" action="" method="POST">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Email" required autofocus>
+              </div>
+              <div class="form-group">
+                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                  Entrar
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+
